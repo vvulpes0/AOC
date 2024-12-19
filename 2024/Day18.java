7cf2aa09a2f44b258d56428cda23824b41cdbb11
Dakotah Lambert <49007668+vvulpes0@users.noreply.github.com>
2024-12-18 22:32:56 -0500

    improve day 18

--- 2024/Day18.java
+++ 2024/Day18.java
@@ -9,10 +9,16 @@ class Day18 {
 	private static final int firstN = 1024;
 	public static void main(String[] args) {
 		ArrayList<ArrayList<Boolean>> map = new ArrayList<ArrayList<Boolean>>();
+		Partition<Integer> mapp = new Partition<Integer>();
+		mapp.add(-1); // TOP
+		mapp.add(-2); // LEFT
+		mapp.add(-3); // RIGHT
+		mapp.add(-4); // BOTTOM
 		for (int i = 0; i < size; i++) {
 			ArrayList<Boolean> bs = new ArrayList<Boolean>();
 			for (int j = 0; j < size; j++) {
 				bs.add(true);
+				mapp.add(v(j,i));
 			}
 			map.add(bs);
 		}
@@ -26,10 +32,23 @@ class Day18 {
 			x = in.nextInt();
 			y = in.nextInt();
 			map.get(y).set(x,false);
+			if (x == 0) mapp.union(v(x,y), -2);
+			if (x+1 == size) mapp.union(v(x,y), -3);
+			if (y == 0) mapp.union(v(x,y), -1);
+			if (y+1 == size) mapp.union(v(x,y), -4);
+			if (y>0 && !map.get(y-1).get(x))
+				mapp.union(v(x,y), v(x,y-1));
+			if (y+1<size && !map.get(y+1).get(x))
+				mapp.union(v(x,y), v(x,y+1));
+			if (x>0 && !map.get(y).get(x-1))
+				mapp.union(v(x,y), v(x-1,y));
+			if (x+1<size && !map.get(y).get(x+1))
+				mapp.union(v(x,y), v(x+1,y));
 			nr++;
-			Integer m = goA(map);
-			if (nr == firstN) partA = m;
-			if (m == null) break;
+			if (nr == firstN) partA = goA(map);
+			if (mapp.get(-1) == mapp.get(-2)) break;
+			if (mapp.get(-1) == mapp.get(-4)) break;
+			if (mapp.get(-2) == mapp.get(-3)) break;
 		}
 		System.out.println(partA + "\t" + x + "," + y);
 	}
@@ -87,6 +106,30 @@ class Day18 {
 				}
 			}
 			return out;
+		}
+	}
+	static class Partition<E> {
+		private HashMap<E,E> parents;
+		Partition() { parents = new HashMap<E,E>(); }
+		boolean add(E e) {
+			if (parents.containsKey(e)) return false;
+			parents.put(e,e);
+			return true;
+		}
+		void clear() { parents.clear(); }
+		boolean isEmpty() { return parents.isEmpty(); }
+		E get(E e) {
+			E p = parents.get(e);
+			if (p == null || p == e) return p;
+			parents.put(e,get(p));
+			return parents.get(e);
+		}
+		boolean union(E x, E y) {
+			add(x);
+			add(y);
+			if (get(x) == get(y)) return false;
+			parents.put(get(y),get(x));
+			return true;
 		}
 	}
 }
